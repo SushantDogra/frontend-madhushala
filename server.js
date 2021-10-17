8e757e18199387fc437241147dcb5232ffb56d78
SushantDogra <sushantsingh.dogra@gmail.com>
2021-10-17 14:14:29 +0530

    heroku setup

---
+++ server.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
+const express = require("express");
+const PORT = 4200;
+const app = express();
+
+app.use(requireHTTPS);
+app.use(express.static("./dist/frontend-madhushala"));
+
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/frontend-madhushala/" })
+);
+
+const listeningOnPort = process.env.PORT || PORT;
+app.listen(listeningOnPort);
+
+console.log(
+  `frontend-madhushala started successfully. listening on port: ${listeningOnPort}`
+);
